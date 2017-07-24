32a5408ed93e76f6bb33704e75b912c49f462a59
Simon Larsén <slarse@kth.se>
2017-07-24 13:56:16 +0200

    Refactor main function

--- pdfebc_cli/main.py
+++ pdfebc_cli/main.py
@@ -30,6 +30,7 @@ Please open an issue about this error at 'https://github.com/slarse/pdfebc/issue
 OUT_DIR_IS_FILE = """The specified output directory ({}) is a file!
 Please specify a path to either an existing directory, or to where you wish to create one."""
 
+CLEANING = "Cleaning up ..."
 
 def main():
     """Run PDFEBC."""
@@ -40,28 +41,64 @@ def main():
         sys.exit(1)
     args = parser.parse_args()
     if args.configstatus:
-        cli.diagnose_config()
-        sys.exit(0)
-    if os.path.isfile(args.outdir):
-        cli.status_callback(OUT_DIR_IS_FILE.format(args.outdir))
-        sys.exit(1)
-    if not os.path.isdir(args.outdir):
-        os.makedirs(args.outdir)
+        diagnose_config_and_exit()
+    exit_if_path_is_file(args.outdir)
+    makedirs_if_path_does_not_exist(args.outdir)
     filepaths = compress_files(args.srcdir, args.outdir, args.ghostscript)
     if args.email:
-        if not config_utils.valid_config_exists():
-            # TODO Add step-by-step config creation here.
-            pass
-        try:
-            loop = asyncio.get_event_loop()
-            loop.run_until_complete(email_supervisor(filepaths))
-            print(email_utils.FILES_SENT)
-        except smtplib.SMTPAuthenticationError as e:
-            cli.status_callback(AUTH_ERROR.format(e.smtp_code, e.smtp_error))
-        except Exception as e:
-            cli.status_callback(UNEXPECTED_ERROR.format(repr(e)))
+        handle_email(filepaths)
     if args.clean:
-        shutil.rmtree(args.outdir)
+        clean_output(args.outdir)
+
+def diagnose_config_and_exit():
+    """Diagnose the config if the corresponding cli option is set, and then exit."""
+    cli.diagnose_config()
+    sys.exit(0)
+
+def exit_if_path_is_file(outdir):
+    """Check if the output directory is a file. If it is, exit the program and
+    print an error message.
+
+    Args:
+        outdir (str): Path to the output directory.
+    """
+    if os.path.isfile(outdir):
+        cli.status_callback(OUT_DIR_IS_FILE.format(outdir))
+        sys.exit(1)
+
+def makedirs_if_path_does_not_exist(path):
+    """Create the directory and any missing intermediate directories,
+    if the path does not exist.
+
+    Args:
+        path (str): Path to a directory.
+    """
+    if not os.path.isdir(path):
+        os.makedirs(path)
+
+def handle_email(filepaths):
+    """Handle everything related to the email argument.
+
+    Args:
+        email (bool): A boolean value.
+        filepaths (List[str]): A list of filepaths.
+    """
+    if not config_utils.valid_config_exists():
+        # TODO Add step-by-step config creation here.
+        pass
+    try:
+        cli.status_callback(send_files_status_message(filepaths))
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(email_supervisor(filepaths))
+    except smtplib.SMTPAuthenticationError as e:
+        cli.status_callback(AUTH_ERROR.format(e.smtp_code, e.smtp_error))
+    except Exception as e:
+        cli.status_callback(UNEXPECTED_ERROR.format(repr(e)))
+    
+def clean_output(outdir):
+    """Print a status message and remove the output directory."""
+    cli.status_callback(CLEANING)
+    shutil.rmtree(outdir)
 
 def compress_files(srcdir, outdir, ghostscript):
     """Compress the PDF files in srcdir and put the results in outdir.
@@ -82,15 +119,15 @@ def compress_files(srcdir, outdir, ghostscript):
     return filepaths
 
 async def working_animation(msg):
-    """Print an animation to stdout.
+    """cli.status_callback an animation to stdout.
 
     Args:
         msg (str): A message to come after the animation.
     """
     write, flush = sys.stdout.write, sys.stdout.flush
-    erase = lambda s: write('\x08' * len(s))
-    right_arrows = reversed(sorted(set(itertools.permutations('>     '))))
-    left_arrows = sorted(set(itertools.permutations('<     ')))
+    erase = lambda s: write('\x08'*len(s))
+    right_arrows = reversed(sorted(set(itertools.permutations('>' + ' '*4))))
+    left_arrows = sorted(set(itertools.permutations('<' + ' '*4)))
     arrow_bars = itertools.chain(right_arrows, left_arrows)
     for arrow_bar in itertools.cycle(arrow_bars):
         status = ''.join(arrow_bar) + ' ' + msg
@@ -103,27 +140,32 @@ async def working_animation(msg):
             break
     erase(status)
 
-async def send_files(filepaths):
-    """Send the files with the settings in the config.
-
-    Args:
-        filepaths List[str]: A list of filepaths.
+def send_files_status_message(filepaths):
+    """Returns:
+        str: The send file status message.
     """
     config = config_utils.read_config()
     args = (email_utils.get_attribute_from_config(config, email_utils.EMAIL_SECTION_KEY, attribute)
             for attribute in [email_utils.USER_KEY, email_utils.RECEIVER_KEY,
                               email_utils.SMTP_SERVER_KEY, email_utils.SMTP_PORT_KEY])
     args = itertools.chain(args, ['\n'.join(filepaths)])
-#    print(email_utils.SENDING_PRECONF.format(*args))
+    return email_utils.SENDING_PRECONF.format(*args)
+
+async def send_files(filepaths):
+    """Send the files with the settings in the config.
+
+    Args:
+        filepaths List[str]: A list of filepaths.
+    """
     await email_utils.send_files_preconf(filepaths)
-#    print(email_utils.FILES_SENT)
+    cli.status_callback(email_utils.FILES_SENT)
 
 async def supervisor(work_func, animation_func):
     """Show the animation on standard out while the async function runs.
 
     Args:
         work_func (asyncio.coroutine): Any asynchronous function.
-        animation_func (asyncio.coroutine): An asynchronous function that prints an animation
+        animation_func (asyncio.coroutine): An asynchronous function that cli.status_callbacks an animation
         to stdout.
     """
     animation = asyncio.ensure_future(animation_func())
@@ -134,9 +176,6 @@ async def email_supervisor(filepaths):
     animation = asyncio.ensure_future(working_animation('Sending files ...'))
     await send_files(filepaths)
     animation.cancel()
-
-async def slow_func():
-    await asyncio.sleep(3)
 
 if __name__ == '__main__':
     main()
